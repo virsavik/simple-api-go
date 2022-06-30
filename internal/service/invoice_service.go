abc5e03236b03d0d806caef6411618e34ca52fc9
virsavik <dhuuloc8818@gmail.com>
2022-06-30 14:09:30 +0700

    Write comment for GenCSVReportByDuration function

--- internal/service/invoice_service.go
+++ internal/service/invoice_service.go
@@ -34,41 +34,53 @@ func (inv InvoiceService) GetAllByDuration(from time.Time, to time.Time) ([]mode
 	return invoices, nil
 }
 
+// Input is used to send data to workers
 type Input struct {
 	Idx     int
 	Invoice model.Invoice
 }
 
+// Record is used to receive data from workers
 type Record struct {
 	Idx  int
 	Data string
 }
 
 func (inv InvoiceService) GenCSVReportByDuration(from time.Time, to time.Time) ([]byte, error) {
-	// 1. Get all invoices by duration sorted by created_at
+	// STEP 1.
+	// Get all invoices by duration sorted by created_at
 	invoices, err := inv.InvoiceRepository.GetAllByDuration(from, to)
 	if err != nil {
 		return nil, err
 	}
 
+	// STEP 1.2
+	// Return error ERR_NO_DATA if no invoices found
 	invoiceLen := len(invoices)
 	if invoiceLen == 0 {
 		return nil, fmt.Errorf(errors.ERR_NO_DATA)
 	}
 
-	// TODO: Find a better way to choose the worker number and buffSize
+	// STEP 2.
+	// Define worker number and buffer size
 	workerNum := 4
 	buffSize := workerNum
-	var wg sync.WaitGroup
 
+	// STEP 3.
+	//Create channels for sending data to workers and receiving data from workers
 	invCh := make(chan Input, buffSize)
 	recordCh := make(chan Record, buffSize)
 
+	// STEP 4.
+	// Create workers
+	var wg sync.WaitGroup
 	wg.Add(workerNum)
 	for i := 0; i < workerNum; i++ {
 		go writeCSVWorker(i, invCh, recordCh, &wg)
 	}
 
+	// STEP 5.
+	// Send data to workers
 	go func() {
 		for i := 0; i < invoiceLen; i++ {
 			invCh <- Input{i, invoices[i]}
@@ -77,22 +89,30 @@ func (inv InvoiceService) GenCSVReportByDuration(from time.Time, to time.Time) (
 		close(invCh)
 	}()
 
+	// STEP 6.
+	// Receive data from workers
 	sortedRecords := make([]string, invoiceLen, invoiceLen)
 	for i := 0; i < invoiceLen; i++ {
 		record := <-recordCh
 		sortedRecords[record.Idx] = record.Data
 	}
 
+	// STEP 7.
+	// Wait for all workers to finish
 	wg.Wait()
 
 	// Be careful big-O of strings.Join
 	return []byte(strings.Join(sortedRecords, "\n")), nil
 }
 
+// toString converts Invoice to csv record as string
 func toString(invoice model.Invoice) string {
 	return invoice.ID + "," + invoice.StorekeeperID + "," + invoice.CreatedAt.Format("2006-01-02 15:04:05") + "," + invoice.UpdatedAt.Format("2006-01-02 15:04:05")
 }
 
+// writeCSVWorker is a worker that
+// receives data from invCh and converts Invoice to csv record
+// then sends data to recordCh
 func writeCSVWorker(id int, inpCh <-chan Input, recordCh chan<- Record, wg *sync.WaitGroup) {
 	for in := range inpCh {
 		recordCh <- Record{in.Idx, toString(in.Invoice)}
