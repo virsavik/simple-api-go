f6631a6e9323b497dbc00211b8b5729f4774d6ee
virsavik <dhuuloc8818@gmail.com>
2022-06-30 13:59:26 +0700

    Generate csv with go routine

--- internal/service/invoice_service.go
+++ internal/service/invoice_service.go
@@ -5,6 +5,7 @@ import (
 	"gokiosk/internal/errors"
 	"gokiosk/internal/model"
 	"gokiosk/internal/repository"
+	"strings"
 	"sync"
 	"time"
 )
@@ -19,9 +20,9 @@ func NewInvoiceService(invoiceRepository repository.IInvoiceRepository) InvoiceS
 	}
 }
 
-func (i InvoiceService) GetAllByDuration(from time.Time, to time.Time) ([]model.Invoice, error) {
+func (inv InvoiceService) GetAllByDuration(from time.Time, to time.Time) ([]model.Invoice, error) {
 	// 1. Get all invoices
-	invoices, err := i.InvoiceRepository.GetAllByDuration(from, to)
+	invoices, err := inv.InvoiceRepository.GetAllByDuration(from, to)
 	if err != nil {
 		return nil, err
 	}
@@ -33,9 +34,19 @@ func (i InvoiceService) GetAllByDuration(from time.Time, to time.Time) ([]model.
 	return invoices, nil
 }
 
-func (i InvoiceService) GenCSVReportByDuration(from time.Time, to time.Time) ([]byte, error) {
-	// 1. Get all invoices
-	invoices, err := i.InvoiceRepository.GetAllByDuration(from, to)
+type Input struct {
+	Idx     int
+	Invoice model.Invoice
+}
+
+type Record struct {
+	Idx  int
+	Data string
+}
+
+func (inv InvoiceService) GenCSVReportByDuration(from time.Time, to time.Time) ([]byte, error) {
+	// 1. Get all invoices by duration sorted by created_at
+	invoices, err := inv.InvoiceRepository.GetAllByDuration(from, to)
 	if err != nil {
 		return nil, err
 	}
@@ -45,65 +56,72 @@ func (i InvoiceService) GenCSVReportByDuration(from time.Time, to time.Time) ([]
 		return nil, fmt.Errorf(errors.ERR_NO_DATA)
 	}
 
-	buffSize := 20 // TODO: Find a better way to choose the buffer size
-	workerNum := 4 // TODO: Find a better way to choose the worker number
+	// TODO: Find a better way to choose the worker number and buffSize
+	workerNum := 4
+	buffSize := workerNum
+	var wg sync.WaitGroup
 
-	dataCh := make(chan []byte, buffSize)
-	defer close(dataCh)
+	invCh := make(chan Input, buffSize)
+	recordCh := make(chan Record, buffSize)
 
-	wg := sync.WaitGroup{}
 	wg.Add(workerNum)
-
-	var csvData []byte
-
 	for i := 0; i < workerNum; i++ {
-		lo := i * invoiceLen / workerNum
-		hi := (i + 1) * invoiceLen / workerNum
-		go writeCsvWorker(invoices[lo:hi], dataCh, &wg)
+		go writeCSVWorker(i, invCh, recordCh, &wg)
 	}
 
-	// TODO: Is this goroutine ?
+	go func() {
+		for i := 0; i < invoiceLen; i++ {
+			invCh <- Input{i, invoices[i]}
+		}
+		// Close the channel when all invoices are sent
+		close(invCh)
+	}()
+
+	sortedRecords := make([]string, invoiceLen, invoiceLen)
 	for i := 0; i < invoiceLen; i++ {
-		csvData = append(csvData, <-dataCh...)
+		record := <-recordCh
+		sortedRecords[record.Idx] = record.Data
 	}
 
 	wg.Wait()
 
-	return csvData, nil
+	// Be careful big-O of strings.Join
+	return []byte(strings.Join(sortedRecords, "\n")), nil
 }
 
 func toString(invoice model.Invoice) string {
 	return invoice.ID + "," + invoice.StorekeeperID + "," + invoice.CreatedAt.Format("2006-01-02 15:04:05") + "," + invoice.UpdatedAt.Format("2006-01-02 15:04:05")
 }
 
-func writeCsvWorker(invoices []model.Invoice, dataCh chan<- []byte, wg *sync.WaitGroup) {
-	for _, invoice := range invoices {
-		dataCh <- []byte(toString(invoice) + "\n")
+func writeCSVWorker(id int, inpCh <-chan Input, recordCh chan<- Record, wg *sync.WaitGroup) {
+	for in := range inpCh {
+		recordCh <- Record{in.Idx, toString(in.Invoice)}
 	}
+	fmt.Printf("Worker %d done\n", id)
 	wg.Done()
 }
 
-func (i InvoiceService) GetAllByPaginate(offset, limit int) ([]model.Invoice, error) {
+func (inv InvoiceService) GetAllByPaginate(offset, limit int) ([]model.Invoice, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i InvoiceService) GetByID(id string) (model.Invoice, error) {
+func (inv InvoiceService) GetByID(id string) (model.Invoice, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i InvoiceService) Create(invoice model.Invoice) (model.Invoice, error) {
+func (inv InvoiceService) Create(invoice model.Invoice) (model.Invoice, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i InvoiceService) Update(id string, invoice model.Invoice) (model.Invoice, error) {
+func (inv InvoiceService) Update(id string, invoice model.Invoice) (model.Invoice, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i InvoiceService) DeleteByID(id string) error {
+func (inv InvoiceService) DeleteByID(id string) error {
 	//TODO implement me
 	panic("implement me")
 }
